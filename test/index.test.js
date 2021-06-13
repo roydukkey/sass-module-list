30b382d61c66328eabec88a96f783b5c57d2ecc9
roydukkey <contact@changelog.me>
2021-06-12 23:24:14 -0400

    Add unit testing for `@error`s

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,38 @@
 // Copyright (c) roydukkey. All rights reserved.
 // Licensed under the MIT. See LICENSE file in the project root for full license information.
 
-const path = require('path');
+const fs = require('fs-extra');
+const replace = require('replace-in-file');
 const sass = require('sass');
 const sassTrue = require('sass-true');
 
+
+// Configuration
+const entry = './test/index.sass';
+const source = './src';
 const sassConfig = {
-	file: path.join(__dirname, 'index.sass'),
+	file: entry,
 	includePaths: ['node_modules']
 };
+const backupSlug = '.testing-backup';
 
-sassTrue.runSass(sassConfig, { sass, describe, it });
+
+// Back up source and transform `@error`s to `true` error handling
+fs.copySync(source, `${source}${backupSlug}`);
+replace.sync({
+	files: `${source}/**/*.sass`,
+	from: [/(.)/, /^(\s*)@error\s(.*)$/gm],
+	to: ["@use 'throw'\n$1", '$1@return throw.error($2)'],
+});
+
+
+// Run tests
+try {
+	sassTrue.runSass(sassConfig, { sass, describe, it });
+}
+
+
+// Restore source
+finally {
+	fs.moveSync(`${source}${backupSlug}`, source, { overwrite: true })
+}
